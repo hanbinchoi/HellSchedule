c59a2ba6d272301620f10f4806a66f50b438ab66
TaewonBae <43931412+TaewonBae@users.noreply.github.com>
2021-08-30 20:23:25 +0900

    Update MainActivity.java

--- app/src/main/java/hellschedule/hellschedule/MainActivity.java
+++ app/src/main/java/hellschedule/hellschedule/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Toolbar 설정1
+        //Toolbar 설정11
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
